b4d380f7aa9e0f21519c16e29ca8239154329b7d
边上海 <bshsf@qq.com>
2022-10-25 17:30:44 +0800

    [change] 跨版本兼容问题优化，菜单新增文档
    1. 添加 EditorStatusWatcher 解决 git 等外部修改了配置不能同步的问题。
    2. Reset 按钮 icon 2021版本有区别，同步之

--- Editor/Settings/MenuProvider.cs
+++ Editor/Settings/MenuProvider.cs
@@ -5,9 +5,17 @@ public static class MenuProvider
 {
     [MenuItem("HybridCLR/Settings", priority = 200)]
     public static void OpenSettings() => SettingsService.OpenProjectSettings("Project/HybridCLR Settings");
-    [MenuItem("HybridCLR/About us", priority = 400)]
-    public static void AboutUs() => Application.OpenURL("https://focus-creative-games.github.io/hybridclr/about/");
-
-    [MenuItem("HybridCLR/Documents", priority = 399)]
-    public static void OpenDoc() => Application.OpenURL("https://focus-creative-games.github.io/hybridclr/");
+    [MenuItem("HybridCLR/Documents/", menuItem = "HybridCLR/Documents/Quick Start")]
+    public static void OpenQuickStart() => Application.OpenURL("https://focus-creative-games.github.io/hybridclr/start_up/");
+    [MenuItem("HybridCLR/Documents/", menuItem = "HybridCLR/Documents/Benchmark")]
+    public static void OpenBenchmark() => Application.OpenURL("https://focus-creative-games.github.io/hybridclr/benchmark/");
+    [MenuItem("HybridCLR/Documents/", menuItem = "HybridCLR/Documents/FAQ")]
+    public static void OpenFAQ() => Application.OpenURL("https://focus-creative-games.github.io/hybridclr/faq/");
+    [MenuItem("HybridCLR/Documents/", menuItem = "HybridCLR/Documents/Common Errors")]
+    public static void OpenCommonErrors() => Application.OpenURL("https://focus-creative-games.github.io/hybridclr/common_errors/");
+    [MenuItem("HybridCLR/Documents/", menuItem = "HybridCLR/Documents/Bug Report")]
+    public static void OpenBugReport() => Application.OpenURL("https://focus-creative-games.github.io/hybridclr/bug_reporter/");
+    [MenuItem("HybridCLR/Documents/", menuItem = "HybridCLR/Documents/About HybridCLR")]
+    public static void OpenAbout() => Application.OpenURL("https://focus-creative-games.github.io/hybridclr/about/");
 }
+

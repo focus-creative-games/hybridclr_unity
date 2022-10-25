1b69c9ca78c487f3bd92c5fd76e0d1f71e220c74
ForeseePretty <huangchaoqun@users.noreply.github.com>
2022-10-25 17:26:21 +0800

    [fix]:支持hybridclr与il2cpp_plus仓库更新 (#11)
    * fix: Data~文件读取修正
    
    * [fix]:支持hybridclr与il2cpp_plus仓库更新

---
+++ Editor/Installer/UpdateController.cs
@@ -0,0 +1,36 @@
+﻿namespace HybridCLR.Editor.Installer
+{
+    public partial class InstallerController
+    {
+        public bool HasUpdateIl2Cpp(string il2cppBranch)
+        {
+            string workDir = SettingsUtil.HybridCLRDataDir;
+            // last hash hybridclr
+            string hybridclrRepoDir = $"{workDir}/{hybridclr_repo_path}";
+            {
+                var ret1 = BashUtil.RunCommand2(hybridclrRepoDir, "git",
+                    new string[] { "log", "HEAD", "-n", "1", "--pretty=format:\"%H\"", });
+                var ret2 = BashUtil.RunCommand2(hybridclrRepoDir, "git",
+                    new string[] { "log", "remotes/origin/HEAD", "-n", "1", "--pretty=format:\"%H\"", });
+                if (ret1.StdOut != ret2.StdOut)
+                {
+                    return true;
+                }
+            }
+
+            // last hash il2cpp_plus
+            string il2cppPlusRepoDir = $"{workDir}/{il2cpp_plus_repo_path}";
+            {
+                var ret1 = BashUtil.RunCommand2(il2cppPlusRepoDir, "git",
+                    new string[] { "log", $"{il2cppBranch}", "-n", "1", "--pretty=format:\"%H\"", });
+                var ret2 = BashUtil.RunCommand2(il2cppPlusRepoDir, "git",
+                    new string[] { "log", $"remotes/origin/{il2cppBranch}", "-n", "1", "--pretty=format:\"%H\"", });
+                if (ret1.StdOut != ret2.StdOut)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}

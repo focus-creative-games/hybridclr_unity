8f12fc965dc3dff6e5569c33a5718e5168c28842
hansheng <1091225802@qq.com>
2022-10-27 16:11:18 +0800

    [fix]:修正hybridclr与il2cpp_plus仓库更新检测的fetch深度

--- Editor/Installer/UpdateController.cs
+++ Editor/Installer/UpdateController.cs
@@ -6,10 +6,12 @@
         {
             string workDir = SettingsUtil.HybridCLRDataDir;
             // last hash hybridclr
-            string hybridclrRepoDir = $"{workDir}/{hybridclr_repo_path}";
             {
+                string hybridclrRepoDir = $"{workDir}/{hybridclr_repo_path}";
                 var ret1 = BashUtil.RunCommand2(hybridclrRepoDir, "git",
                     new string[] { "log", "HEAD", "-n", "1", "--pretty=format:\"%H\"", });
+                BashUtil.RunCommand2(hybridclrRepoDir, "git",
+                    new string[] { "fetch", "depth=1" });
                 var ret2 = BashUtil.RunCommand2(hybridclrRepoDir, "git",
                     new string[] { "log", "remotes/origin/HEAD", "-n", "1", "--pretty=format:\"%H\"", });
                 if (ret1.StdOut != ret2.StdOut)
@@ -17,12 +19,13 @@
                     return true;
                 }
             }
-
             // last hash il2cpp_plus
-            string il2cppPlusRepoDir = $"{workDir}/{il2cpp_plus_repo_path}";
             {
+                string il2cppPlusRepoDir = $"{workDir}/{il2cpp_plus_repo_path}";
                 var ret1 = BashUtil.RunCommand2(il2cppPlusRepoDir, "git",
                     new string[] { "log", $"{il2cppBranch}", "-n", "1", "--pretty=format:\"%H\"", });
+                BashUtil.RunCommand2(il2cppPlusRepoDir, "git",
+                    new string[] { "fetch", "depth=1" });
                 var ret2 = BashUtil.RunCommand2(il2cppPlusRepoDir, "git",
                     new string[] { "log", $"remotes/origin/{il2cppBranch}", "-n", "1", "--pretty=format:\"%H\"", });
                 if (ret1.StdOut != ret2.StdOut)

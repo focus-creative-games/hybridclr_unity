419c3a98f644417505aba867e5e95190a4eb9942
walon <walon@focus-creative-games.com>
2022-12-06 18:57:48 +0800

    [opt] installer安装成功后会记录 安装的版本，并且显示。

--- Editor/Installer/InstallerWindow.cs
+++ Editor/Installer/InstallerWindow.cs
@@ -10,12 +10,22 @@ namespace HybridCLR.Editor.Installer
     {
         private InstallerController _controller;
 
-        string _hybridclrVersion = "";
-        string _il2cppPlusVersion = "";
+        string _hybridclrVersion;
+        string _il2cppPlusVersion;
 
         private void OnEnable()
         {
             _controller = new InstallerController();
+            _hybridclrVersion = _controller.HybridCLRRepoInstalledVersion;
+            if (string.IsNullOrWhiteSpace(_hybridclrVersion))
+            {
+                _hybridclrVersion = "main";
+            }
+            _il2cppPlusVersion = _controller.Il2CppRepoInstalledVersion;
+            if (string.IsNullOrWhiteSpace(_il2cppPlusVersion))
+            {
+                _il2cppPlusVersion = $"{_controller.MajorVersion}-main";
+            }
         }
 
         private void OnGUI()
@@ -87,6 +97,14 @@ namespace HybridCLR.Editor.Installer
             {
                 Debug.LogError($"il2cpp 版本不兼容，最小版本为 {_controller.GetCurrentUnityVersionMinCompatibleVersionStr()}");
                 return;
+            }
+            if (string.IsNullOrWhiteSpace(_hybridclrVersion))
+            {
+                _hybridclrVersion = "main";
+            }
+            if (string.IsNullOrWhiteSpace(_il2cppPlusVersion))
+            {
+                _il2cppPlusVersion = $"{_controller.MajorVersion}-main";
             }
             _controller.InstallLocalHybridCLR(_hybridclrVersion, _il2cppPlusVersion);
         }

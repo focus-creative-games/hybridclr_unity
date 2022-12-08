ef0db32fc1aab348d23b074a034a82597ce8bc19
walon <walon@focus-creative-games.com>
2022-12-08 22:19:27 +0800

    [fix] 修复生成桥接函数时不必要地多次调用 CleanIl2CppBuildCache，并有可能因并发清除而抛出异常的问题

--- Editor/Commands/MethodBridgeGeneratorCommand.cs
+++ Editor/Commands/MethodBridgeGeneratorCommand.cs
@@ -42,7 +42,6 @@ namespace HybridCLR.Editor.Commands
             g.PrepareMethods();
             g.Generate();
             Debug.LogFormat("== output:{0} ==", outputFile);
-            CleanIl2CppBuildCache();
         }
 
         [MenuItem("HybridCLR/Generate/MethodBridge", priority = 101)]
@@ -83,6 +82,7 @@ namespace HybridCLR.Editor.Commands
                 Task.WaitAll(tasks.ToArray());
             }
 
+            CleanIl2CppBuildCache();
         }
     }
 }

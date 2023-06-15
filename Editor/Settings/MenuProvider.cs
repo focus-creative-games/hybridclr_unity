c8cd323bb0562dc80d5944e10914d8b9f7d80256
walon <walon@focus-creative-games.com>
2023-06-15 17:06:55 +0800

    [fix] 修复重构文档站后的帮助文档的链接错误

--- Editor/Settings/MenuProvider.cs
+++ Editor/Settings/MenuProvider.cs
@@ -6,7 +6,7 @@ public static class MenuProvider
 {
 
     [MenuItem("HybridCLR/About HybridCLR", priority = 0)]
-    public static void OpenAbout() => Application.OpenURL("https://hybridclr.doc.code-philosophy.com/#/README");
+    public static void OpenAbout() => Application.OpenURL("https://hybridclr.doc.code-philosophy.com/docs/intro");
 
     [MenuItem("HybridCLR/Installer...", priority = 60)]
     private static void Open()
@@ -19,18 +19,18 @@ public static class MenuProvider
     public static void OpenSettings() => SettingsService.OpenProjectSettings("Project/HybridCLR Settings");
 
     [MenuItem("HybridCLR/Documents/Quick Start")]
-    public static void OpenQuickStart() => Application.OpenURL("https://hybridclr.doc.code-philosophy.com/#/beginner/quickstart");
+    public static void OpenQuickStart() => Application.OpenURL("https://hybridclr.doc.code-philosophy.com/docs/beginner/quickstart");
 
-    [MenuItem("HybridCLR/Documents/Benchmark")]
-    public static void OpenBenchmark() => Application.OpenURL("https://hybridclr.doc.code-philosophy.com/#/basic/performance");
+    [MenuItem("HybridCLR/Documents/Performance")]
+    public static void OpenPerformance() => Application.OpenURL("https://hybridclr.doc.code-philosophy.com/docs/basic/performance");
 
     [MenuItem("HybridCLR/Documents/FAQ")]
-    public static void OpenFAQ() => Application.OpenURL("https://hybridclr.doc.code-philosophy.com/#/help/faq");
+    public static void OpenFAQ() => Application.OpenURL("https://hybridclr.doc.code-philosophy.com/docs/help/faq");
 
     [MenuItem("HybridCLR/Documents/Common Errors")]
-    public static void OpenCommonErrors() => Application.OpenURL("https://hybridclr.doc.code-philosophy.com/#/help/commonerrors");
+    public static void OpenCommonErrors() => Application.OpenURL("https://hybridclr.doc.code-philosophy.com/docs/help/commonerrors");
 
     [MenuItem("HybridCLR/Documents/Bug Report")]
-    public static void OpenBugReport() => Application.OpenURL("https://hybridclr.doc.code-philosophy.com/#/help/issue");
+    public static void OpenBugReport() => Application.OpenURL("https://hybridclr.doc.code-philosophy.com/docs/help/issue");
 }
 

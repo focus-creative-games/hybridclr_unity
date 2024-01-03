ef103d83513090519796e57f6d1a237770fb2af0
walon <walon@code-philosophy.com>
2024-01-03 17:30:28 +0800

    [new] 支持团结引擎WeixinMiniGame和OpenHarmony BuildTarget下复制裁剪后的AOT dll

--- Editor/BuildProcessors/CopyStrippedAOTAssemblies.cs
+++ Editor/BuildProcessors/CopyStrippedAOTAssemblies.cs
@@ -45,6 +45,12 @@ namespace HybridCLR.Editor.BuildProcessors
                     return $"{projectDir}/Library/Bee/artifacts/PS4PlayerBuildProgram/ManagedStripped";
                 case BuildTarget.PS5:
                     return $"{projectDir}/Library/Bee/artifacts/PS5PlayerBuildProgram/ManagedStripped";
+#if TUANJIE_2022
+                case BuildTarget.WeixinMiniGame:
+                    return $"{projectDir}/Library/Bee/artifacts/WeixinMiniGame/ManagedStripped";
+                case BuildTarget.OpenHarmony:
+                    return $"{projectDir}/Library/Bee/artifacts/OpenHarmonyPlayerBuildProgram/ManagedStripped";
+#endif
                 default: return "";
             }
         }
